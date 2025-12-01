c13995bd2f9985c02f76a7e2288174e937fa2826
drayanqi <hugo.venancio@hotmail.fr>
2025-12-01 14:24:12 +0100

    Refactor physics engine and possession handling

--- src/sim/Entity.js
+++ src/sim/Entity.js
@@ -1,2 +1,21 @@
-// TODO: implement module based on spec
-export default {};
+export class Entity {
+  constructor({ x = 0, y = 0, radius = 0, mass = 1 } = {}) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.radius = radius;
+    this.mass = mass || 1;
+  }
+
+  get speed() {
+    return Math.hypot(this.vx, this.vy);
+  }
+
+  applyImpulse(ix, iy) {
+    this.vx += ix / this.mass;
+    this.vy += iy / this.mass;
+  }
+}
+
+export default Entity;
--- src/sim/Integrator.js
+++ src/sim/Integrator.js
@@ -1,2 +1,11 @@
-// TODO: implement module based on spec
-export default {};
+export function integrateEuler(entity, dt) {
+  entity.x += entity.vx * dt;
+  entity.y += entity.vy * dt;
+}
+
+export function applyDamping(entity, damping) {
+  entity.vx *= damping;
+  entity.vy *= damping;
+}
+
+export default { integrateEuler, applyDamping };
